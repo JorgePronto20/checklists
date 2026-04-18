d953513347c2a166b7a4fdf90f94a5358c602095
JorgePronto20 <jorgepronto20@hotmail.com>
2026-04-18 15:47:32 +0100

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function validarPIN() {
+async function validarPIN() {
   const pin = document.getElementById("pin").value;
   const erro = document.getElementById("erro");
-  if (pin === "2121") {
-    window.location.href = "dashboard.html";
-  } else {
-    erro.textContent = "PIN inválido";
+
+  if (!pin) {
+    erro.textContent = "Insere o PIN";
+    return;
+  }
+
+  try {
+    const resposta = await fetch(
+      "https://script.google.com/macros/s/AKfycbwxSs_v27UPL9J3UsvCKYFQrBAcJC9fPYWkLfar733rS2GHqgxcdUap5TiXDsxjXUDo/exec?pin=" + pin
+    );
+
+    const data = await resposta.json();
+
+    if (data.valido) {
+      localStorage.setItem("auth", "ok");
+      location.href = "dashboard.html";
+    } else {
+      erro.textContent = "PIN inválido ou inativo";
+    }
+
+  } catch (e) {
+    erro.textContent = "Erro de ligação à API";
   }
 }
